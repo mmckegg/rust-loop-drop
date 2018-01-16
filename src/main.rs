1dd67d408aa7cef9025214839fdb44ef70934ca4
Matt McKegg <matt@wetsand.co.nz>
2018-01-16 18:05:42 +1300

    use virtual midi clock (for testing)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
 use std::io::{stdin};
+use std::thread;
+use std::time;
 
 mod midi_connection;
 mod loop_grid_launchpad;
@@ -18,14 +20,24 @@ fn main() {
 fn run() -> Result<(), Box<Error>> {
     let launchpad = LoopGridLaunchpad::new("Launchpad Mini");
     let launchpad_clock_channel = launchpad.get_channel();
+    let tempo = 120;
 
     let mut ticks = 0;
-    let clock_in = midi_connection::get_input("IAC Driver Bus 1", move |stamp, message, _| {
-        if message[0] == 248 {
-            ticks += 1;
+    // let clock_in = midi_connection::get_input("IAC Driver Bus 1", move |stamp, message, _| {
+    //     if message[0] == 248 {
+    //         ticks += 1;
+    //         launchpad_clock_channel.send(LoopGridMessage::Schedule(ticks));
+    //     }
+    // }, ());
+
+    thread::spawn(move || {
+        loop {
             launchpad_clock_channel.send(LoopGridMessage::Schedule(ticks));
+            ticks += 1;
+            thread::sleep(time::Duration::from_millis(1000 / (tempo / 60) / 24));
         }
-    }, ());
+    });
+
 
     let mut input = String::new();
     stdin().read_line(&mut input)?; // wait for next enter key press
