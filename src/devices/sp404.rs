e6150f4e39536a0257cdbf87570176a67d080179
Matt McKegg <matt@wetsand.co.nz>
2018-05-01 22:15:52 +1200

    sp404: schedule chokes after triggers

--- src/devices/sp404.rs
+++ src/devices/sp404.rs
@@ -3,23 +3,71 @@ use ::midi_connection;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicUsize, Ordering};
 pub use ::scale::Scale;
+use std::sync::mpsc;
+use std::thread;
+use std::collections::HashSet;
 
 use std::collections::HashMap;
 
 pub struct SP404 {
-    midi_port: midi_connection::SharedMidiOutputConnection,
-    midi_channel: u8,
-    output_values: HashMap<u32, (u8, u8, u8)>,
-    offset: Arc<AtomicUsize>
+    tx: mpsc::Sender<SP404Message>
 }
 
 impl SP404 {
-    pub fn new (midi_port: midi_connection::SharedMidiOutputConnection, channel: u8, offset: Arc<AtomicUsize>) -> Self {
+    pub fn new (midi_port: midi_connection::SharedMidiOutputConnection, midi_channel: u8, offset: Arc<AtomicUsize>) -> Self {
+        let (tx, rx) = mpsc::channel();
+
+        let tx_feedback = tx.clone();
+
+        thread::spawn(move || {
+            let mut to_choke = HashSet::new();
+            let mut output_values: HashMap<u32, (u8, u8, u8)> = HashMap::new();
+            let mut midi_port = midi_port;
+
+            for msg in rx {
+                match msg {
+                    SP404Message::Trigger(id, _) => {
+                        let mut offset_value = offset.load(Ordering::Relaxed);
+
+                        let mut channel = if offset_value < 5 {
+                            midi_channel
+                        } else {
+                            midi_channel + 1
+                        };
+
+                        // choke
+                        for (_, &(channel, note_id, _)) in &output_values {
+                            // add to queue so that chokes are processed after note ons
+                            to_choke.insert((channel, note_id));
+                            tx_feedback.send(SP404Message::Choke(channel, note_id)).unwrap();                            
+                        }
+
+                        output_values.clear();
+
+                        let note_id = (47 + ((offset_value % 5) * 12) + (id as usize)) as u8;
+                        midi_port.send(&[144 - 1 + channel, note_id, 127]).unwrap();
+                        output_values.insert(id, (channel, note_id, 127));
+
+                        // since this note has been added, we don't need to choke it any more
+                        to_choke.remove(&(channel, note_id));
+                    },
+                    SP404Message::Choke(channel, note_id) => {
+                        // only trigger this choke if it is still valid
+                        if to_choke.contains(&(channel, note_id)) {
+                            midi_port.send(&[128 - 1 + channel, note_id, 0]).unwrap();
+                        }
+
+                        to_choke.remove(&(channel, note_id));
+                    }
+                }
+            }
+        });
+
+
+
+
         SP404 {
-            midi_port,
-            midi_channel: channel,
-            output_values: HashMap::new(),
-            offset
+            tx
         }
     }
 }
@@ -34,26 +82,14 @@ impl Triggerable for SP404 {
                 //     self.output_values.remove(&id);
                 // }
             },
-            OutputValue::On(_) => {
-                let mut offset_value = self.offset.load(Ordering::Relaxed);
-
-                let mut channel = if offset_value < 5 {
-                    self.midi_channel
-                } else {
-                    self.midi_channel + 1
-                };
-
-                // choke
-                for (_, &(channel, note_id, _)) in &self.output_values {
-                    self.midi_port.send(&[128 - 1 + channel, note_id, 0]).unwrap();
-                }
-
-                self.output_values.clear();
-
-                let note_id = (47 + ((offset_value % 5) * 12) + (id as usize)) as u8;
-                self.midi_port.send(&[144 - 1 + channel, note_id, 127]).unwrap();
-                self.output_values.insert(id, (channel, note_id, 127));
+            OutputValue::On(velocity) => {
+                self.tx.send(SP404Message::Trigger(id, velocity)).unwrap();
             }
         }
     }
+}
+
+enum SP404Message {
+    Choke(u8, u8),
+    Trigger(u32, u8)
 }
